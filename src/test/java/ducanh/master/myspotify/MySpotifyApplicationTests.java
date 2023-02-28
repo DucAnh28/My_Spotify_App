8cd04ad46c0b1e1aea25c3f4af83c793193f6541
DucAnh <ducanhnguyeny2k@gmail.com>
2023-02-28 12:21:50 +0700

    first init

---
+++ src/test/java/ducanh/master/myspotify/MySpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package ducanh.master.myspotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
